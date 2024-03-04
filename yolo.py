0b4f38567af5c320a276f0c0994446238102f1c4
AbdElRahman_ElGharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2024-03-04 21:02:38 +0200

    implemented YOLO model compute_loss method

--- yolo.py
+++ yolo.py
@@ -54,6 +54,57 @@ class YOLO(keras.Model):
         x = self.detection_head(x)
         return x
 
+    def compute_loss(self, x=None, y=None, y_pred=None, sample_weight=None):
+        pred_boxes = y_pred['boxes']
+        pred_scores = y_pred['classes']
+
+        pred_boxes = tf.reshape(pred_boxes, shape=(-1, 4, 16))
+        pred_boxes = tf.nn.softmax(logits=pred_boxes, axis=-1) * tf.range(16, dtype='float32')
+        pred_boxes = tf.math.reduce_sum(pred_boxes, axis=-1)
+
+        anchor_points, stride_tensor = get_anchors(image_shape=x.shape[1:])
+        stride_tensor = tf.expand_dims(stride_tensor, axis=-1)
+
+        true_labels = y["classes"]
+        true_boxes = y["boxes"]
+
+        mask_gt = tf.math.reduce_all(true_boxes > -1.0, axis=-1, keepdims=True)
+
+        pred_boxes = dist2bbox(pred_boxes, anchor_points)
+
+        target_boxes, target_scores, fg_mask = self.label_encoder(
+            pred_scores,
+            tf.expand_dims(tf.cast(pred_boxes * stride_tensor, true_boxes.dtype), axis=0),
+            anchor_points * stride_tensor,
+            true_labels,
+            true_boxes,
+            mask_gt,
+        )
+
+        target_boxes /= stride_tensor
+        target_scores_sum = maximum(tf.math.reduce_sum(target_scores), 1)
+        box_weight = tf.expand_dims(
+            tf.math.reduce_sum(target_scores, axis=-1) * fg_mask,
+            axis=-1,
+        )
+
+        y_true = {
+            "box": target_boxes * fg_mask[..., None],
+            "class": target_scores,
+        }
+        y_pred = {
+            "box": pred_boxes * fg_mask[..., None],
+            "class": pred_scores,
+        }
+        sample_weights = {
+            "box": self.box_loss_weight * box_weight / target_scores_sum,
+            "class": self.classification_loss_weight / target_scores_sum,
+        }
+
+        return super().compute_loss(
+            x=x, y=y_true, y_pred=y_pred, sample_weight=sample_weights
+        )
+
     def predict_step(self, *args):
         outputs = super(YOLO, self).predict_step(*args)
 
