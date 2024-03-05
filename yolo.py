e19c67a2d8a9fbe4542eb2ad4a39a8015a1b8879
AbdElRahman_ElGharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2024-03-06 01:06:06 +0200

    moved maximum helper function

--- yolo.py
+++ yolo.py
@@ -1,18 +1,7 @@
 from model import *
 from prediction_decoder import *
 from keras_cv.src.models.object_detection.yolo_v8.yolo_v8_label_encoder import YOLOV8LabelEncoder
-from loss import CIoULoss
-
-
-def maximum(x1, x2):
-    if isinstance(x1, tf.SparseTensor):
-        if isinstance(x2, tf.SparseTensor):
-            return tf.sparse.maximum(x1, x2)
-        else:
-            x1 = tf.sparse.to_dense(x1)
-    elif isinstance(x2, tf.SparseTensor):
-        x2 = tf.sparse.to_dense(x2)
-    return tf.math.maximum(x1, x2)
+from loss import *
 
 
 class YOLO(keras.Model):
