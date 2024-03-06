c983bcc4b1ae2624e90ab2c7b1b0f6f7ef601196
AbdElRahman_ElGharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2024-03-06 13:10:43 +0200

    implemented is_anchor_center_within_box helper function

--- label_encoder.py
+++ label_encoder.py
@@ -2,3 +2,13 @@ import tensorflow as tf
 import keras
 # import tensorflow.keras as keras
 from keras_cv.src.models.object_detection.yolo_v8.yolo_v8_label_encoder import YOLOV8LabelEncoder as LabelEncoder
+
+
+def is_anchor_center_within_box(anchors, gt_bboxes):
+    return tf.math.reduce_all(
+        tf.math.logical_and(
+            gt_bboxes[:, :, None, :2] < anchors,
+            gt_bboxes[:, :, None, 2:] > anchors,
+        ),
+        axis=-1,
+    )
