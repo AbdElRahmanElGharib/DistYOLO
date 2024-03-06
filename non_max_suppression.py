be771c44a10fa982bac2b6f994b9c1253e1f6e01
AbdElRahman_ElGharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2024-03-06 02:24:14 +0200

    implemented NonMaxSuppression class

--- non_max_suppression.py
+++ non_max_suppression.py
@@ -1,4 +1,21 @@
 import tensorflow as tf
 import keras
 # import tensorflow.keras as keras
-from keras_cv.src.layers import NonMaxSuppression
+
+
+class NonMaxSuppression(keras.layers.Layer):
+    def __init__(
+        self,
+        iou_threshold=0.5,
+        confidence_threshold=0.5,
+        max_detections=100,
+        **kwargs,
+    ):
+        super(NonMaxSuppression, self).__init__(**kwargs)
+        self.iou_threshold = iou_threshold
+        self.confidence_threshold = confidence_threshold
+        self.max_detections = max_detections
+        self.built = True
+
+    def call(self, inputs, *args, **kwargs):
+        pass
